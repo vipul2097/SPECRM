f7944ecb5e88a8c6d74283f03f2603e83fb1ab4f
vipul2097 <vipula.a.e@gmail.com>
2023-05-14 21:10:01 +0530

    Initial Commit

--- tealcrm/settings.py
+++ tealcrm/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-"Commenting for ngrok"
+"Commenting for git poll scm through ngrok"
 
 from logging import Formatter, Handler
 from pathlib import Path
